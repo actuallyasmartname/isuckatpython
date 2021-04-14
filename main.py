8e1a5d748f687815e089db8acb127c71803c8096
actuallyasmartname <77011692+actuallyasmartname@users.noreply.github.com>
2021-04-14 09:08:24 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+
+root= tk.Tk()
+
+canvas1 = tk.Canvas(root, width = 300, height = 300)
+canvas1.pack()
+
+def test ():  
+    label1 = tk.Label(root, text= 'testing', fg='green', font=('helvetica', 12, 'bold'))
+    canvas1.create_window(150, 200, window=label1)
+root.mainloop()
